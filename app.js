6cd7def597764ef0e4b00ea0abd053c7e9c8b53c
Hiro <101234747+LOTA016@users.noreply.github.com>
2023-04-06 11:48:27 +0630

    Add files via upload

---
+++ app.js
@@ -0,0 +1,35 @@
+alert("Tell Me Why I am waiting for someone");
+console.log("I Love You");
+console.log("But");
+console.log("You don't love me back");
+
+
+ /*
+ var head = document.getElementById("head");
+ head.style.color  =  "red"; 
+ head.style.fontsize = 65 + "px";  
+*/
+
+
+//comment  are not transplited by transpiler
+
+//change text color to pink
+document.querySelector("#head").style.color = "pink";
+
+
+//assign =  assignment  Operator 
+
+let x = 20;//Number Type
+let name = 'Davin';//String Type 
+console.log(typeof Davin);
+document.querySelector('#head').ineerText = name;
+
+
+/*
+let => variable  Keyword
+name => variable identifier name
+= =>end of statement
+
+let name, 'Davin'  => Operands  
+= => Operator 
+*/ 
---
+++ main.py
@@ -0,0 +1,19 @@
+print("I Love Azura");
+print("She is the only one");
+name= "Azura"
+print ("Hello " + name)
+
+from turtle import *
+
+forward(200)
+
+#make a dimameter 200 semicircle, curving 
+for i in range(100):
+      forward(3.1416)
+      right(1.8)
+
+     
+     
+
+bye()
+ 
